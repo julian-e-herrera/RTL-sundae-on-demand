7a7684e17f6d73707eafaae3d1938707f04bf2f6
Julian Herrera <julianeherreraok@gmail.com>
2022-12-07 17:39:19 -0300

    add topping test

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import ScoopOption from './ScoopOption'
+import ToppingOption from './ToppingOption'
+import Row from 'react-bootstrap/Row'
+
+
+const Options = ({ optionType }) => {
+    //optionType is 'scoops' or 'toppings'
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:3030/${optionType}`)
+            .then(response => setItems(response.data))
+            .catch(error => {
+                //TODO: habndler error response
+            })
+    }, [optionType])
+
+
+    const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption
+
+    const optionItems = items.map(item =>
+        <ItemComponent
+            key={item.name}
+            name={item.name}
+            imagePath={item.imagePath}
+        />
+    )
+
+    return (
+        <Row>{optionItems}</Row>
+    )
+}
+
+export default Options
