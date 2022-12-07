d975f11b3c95e57552ac99e507e07a8441856597
Julian Herrera <julianeherreraok@gmail.com>
2022-12-07 16:28:46 -0300

    mock server worjer setup

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,6 @@
 import SummaryForm from '../SummaryForm'
-import { render, screen, fireEvent } from '@testing-library/react'
-
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 test('Checkbox is unchecked by default', () => {
 
@@ -9,19 +9,39 @@ test('Checkbox is unchecked by default', () => {
     expect(checkbox).not.toBeChecked();
 })
 
-test('Checking checkbox enables button', () => {
-
+test('Checking checkbox enables button', async () => {
+    const user = userEvent.setup()
     render(<SummaryForm />)
     const checkbox = screen.getByRole('checkbox')
-    fireEvent.click(checkbox)
+    await user.click(checkbox)
     expect(checkbox).toBeChecked();
 })
 
-test('Unchecking checkbox again disables button', () => {
-
+test('Unchecking checkbox again disables button', async () => {
+    const user = userEvent.setup()
     render(<SummaryForm />)
     const checkbox = screen.getByRole('checkbox')
     const button = screen.getByRole('button')
-    fireEvent.click(button)
+    await user.click(button)
     expect(checkbox).not.toBeChecked();
+})
+
+test('Popover responds to hover', async () => {
+    const user = userEvent.setup()
+
+    render(<SummaryForm />)
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(/no ice cream will be delivered/i)
+    expect(nullPopover).not.toBeInTheDocument()
+
+    //popover appears on mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i)
+    await user.hover(termsAndConditions)
+
+    const popover = screen.getByText(/no ice cream will actually be delivered/i)
+    expect(popover).toBeInTheDocument()
+
+    //popover disappears when we mouse out 
+    await user.unhover(termsAndConditions)
+    expect(popover).not.toBeInTheDocument()
 })
