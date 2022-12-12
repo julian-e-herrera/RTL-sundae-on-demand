1415da9c676d00fe9c9af09e2b4666d13f13d036
Julian Herrera <julianeherreraok@gmail.com>
2022-12-12 10:50:04 -0300

    use waitFor and reset server handlers

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,20 +3,26 @@ import axios from 'axios'
 import ScoopOption from './ScoopOption'
 import ToppingOption from './ToppingOption'
 import Row from 'react-bootstrap/Row'
+import AlertBanner from '../common/AlertBanner'
 
 
 const Options = ({ optionType }) => {
     //optionType is 'scoops' or 'toppings'
     const [items, setItems] = useState([])
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         axios.get(`http://localhost:3030/${optionType}`)
             .then(response => setItems(response.data))
             .catch(error => {
-                //TODO: habndler error response
+                setError(true)
             })
     }, [optionType])
 
+    if (error)
+    {
+        return <AlertBanner />;
+    }
 
     const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption
 
